e04ab48b4e603e5816d8ffac5e244a20b7b220c0
19Doors <sakaarsri1904@gmail.com>
2025-04-04 23:13:13 +0530

    Fix database

--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -1,3 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import {
   Table,
   TableBody,
@@ -17,71 +31,68 @@ export default async function Database() {
   let jobSummariesJSON = await jobSummaries.json();
 
   return (
-    <Tabs defaultValue="jobDesc">
-      <TabsList>
-        <TabsTrigger value="jobDesc" className="cursor-pointer">jobDescriptions</TabsTrigger>
-        <TabsTrigger value="jobSumm" className="cursor-pointer">jobSummaries</TabsTrigger>
-      </TabsList>
-      <TabsContent value="jobDesc">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-14 font-bold">JobID</TableHead>
-              <TableHead className="w-50 text-left font-bold">Title</TableHead>
-              <TableHead className="text-left font-bold">Description</TableHead>
-            </TableRow>
-          </TableHeader>
+    <div>
+      <Table className="w-1/3 border">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-14 font-bold text-center">JobID</TableHead>
+            <TableHead className="w-50 text-center font-bold">Title</TableHead>
+          </TableRow>
+        </TableHeader>
+      </Table>
+      <div className="">
+        <Table className="w-1/3 border">
+          <TableBody>
+            {jobSummariesJSON.map((job: any, id: number): any => {
+              return (
+                <Sheet key={id}>
+                  <SheetTrigger asChild>
+                    <TableRow key={id}>
+                      <TableCell className="min-w-14 text-center">
+                        {job.jobId}
+                      </TableCell>
+                      <TableCell className="overflow-auto font-bold text-center">
+                        {job.title}
+                      </TableCell>
+                    </TableRow>
+                  </SheetTrigger>
+                  <SheetContent className="">
+                    <SheetHeader>
+                      <SheetTitle>{job.title}</SheetTitle>
+                    </SheetHeader>
+                    <div className="mx-4 flex flex-col gap-y-2">
+                      <div>
+                        <p className="font-bold">Role Description</p>
+                        <p className="text-sm ">{job.description}</p>
+                      </div>
+                      <Separator />
+                      <div>
+                        <p className="font-bold">Educational Qualifications</p>
+                        <p className="text-sm">{job.eduQual}</p>
+                      </div>
+                      <Separator />
+                      <div>
+                        <p className="font-bold">Required Experiences</p>
+                        <p className="text-sm">{job.reqExp}</p>
+                      </div>
+                      <Separator />
+                      <div>
+                        <p className="font-bold">Responsibilities</p>
+                        <p className="text-sm">{job.resp}</p>
+                      </div>
+                      <Separator />
+                      <div>
+                        <p className="font-bold">Key Skills</p>
+                        <p className="text-sm">{job.reqSkills}</p>
+                      </div>
+                    </div>
+                  </SheetContent>
+                </Sheet>
+              );
+            })}
+          </TableBody>
         </Table>
-        <div className="">
-          <Table>
-            <TableBody>
-              {jobDescriptionsJSON.map((job: any, id: number): any => {
-                return (
-                  <TableRow key={id}>
-                    <TableCell className="min-w-14 text-center">{job.jobId}</TableCell>
-                    <TableCell className="overflow-auto font-bold">{job.title}</TableCell>
-                    <TableCell className="max-w-screen overflow-auto">{job.description}</TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </div>
-      </TabsContent>
-      <TabsContent value="jobSumm">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-14 font-bold">JobID</TableHead>
-              <TableHead className="w-50 font-bold">Title</TableHead>
-              <TableHead className="w-100 font-bold">Description</TableHead>
-              <TableHead className="w-50 font-bold">Key Skills</TableHead>
-              <TableHead className="w-50 font-bold">Required Experiences</TableHead>
-              <TableHead className="w-50 font-bold">Educational Qualifications</TableHead>
-              <TableHead className="w-50 font-bold">Resposibilities</TableHead>
-            </TableRow>
-          </TableHeader>
-        </Table>
-        <div className="">
-          <Table>
-            <TableBody>
-              {jobSummariesJSON.map((job: any, id: number): any => {
-                return (
-                  <TableRow key={id}>
-                    <TableCell className="min-w-14 text-center">{job.jobId}</TableCell>
-                    <TableCell className="max-w-50 overflow-auto font-bold">{job.title}</TableCell>
-                    <TableCell className="max-w-100 overflow-auto">{job.description}</TableCell>
-                    <TableCell className="max-w-50 overflow-auto">{job.reqSkills}</TableCell>
-                    <TableCell className="max-w-50 overflow-auto">{job.reqExp}</TableCell>
-                    <TableCell className="max-w-50 overflow-auto">{job.eduQual}</TableCell>
-                    <TableCell className="max-w-50 overflow-auto">{job.resp}</TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </div>
-      </TabsContent>
-    </Tabs>
+      </div>
+    </div>
   );
 }
